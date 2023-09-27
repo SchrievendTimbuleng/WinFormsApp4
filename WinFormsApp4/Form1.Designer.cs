eea4657fbf99e6febcc82821fabd53e932eedaeb
Schrievend <Schrievend@DESKTOP-5HC110S>
2023-09-28 00:16:31 +0800

    Add project files.

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            numericUpDown4 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown1 = new NumericUpDown();
+            comboBox4 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(738, 417);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 21);
+            button1.TabIndex = 1;
+            button1.Text = "Next";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(678, 417);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 21);
+            button2.TabIndex = 2;
+            button2.Text = "Back";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(746, 425);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ButtonHighlight;
+            groupBox1.Controls.Add(numericUpDown4);
+            groupBox1.Controls.Add(numericUpDown3);
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(comboBox4);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Location = new Point(263, 122);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(320, 246);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Menu";
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(135, 184);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(41, 23);
+            numericUpDown4.TabIndex = 12;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(135, 138);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(41, 23);
+            numericUpDown3.TabIndex = 11;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(135, 90);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(41, 23);
+            numericUpDown2.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(135, 44);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(41, 23);
+            numericUpDown1.TabIndex = 9;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(6, 184);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(103, 23);
+            comboBox4.TabIndex = 8;
+            comboBox4.Text = "Es Coklat";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(6, 138);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(103, 23);
+            comboBox3.TabIndex = 7;
+            comboBox3.Text = "Es Kepal";
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(6, 90);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(103, 23);
+            comboBox2.TabIndex = 6;
+            comboBox2.Text = "Nutrisari";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(6, 44);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(103, 23);
+            comboBox1.TabIndex = 5;
+            comboBox1.Text = "Air Mineral";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Harlow Solid Italic", 48F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(276, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(291, 81);
+            label3.TabIndex = 6;
+            label3.Text = "Minuman\r\n";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            BackgroundImage = Properties.Resources.WhatsApp_Image_2023_09_27_at_9_04_34_PM;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "t";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private GroupBox groupBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox4;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
